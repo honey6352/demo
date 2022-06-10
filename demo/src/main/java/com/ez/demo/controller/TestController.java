f2571b4bb446d3a26cc848f2ef2a56c6a575745b
EZEN <EZEN@DESKTOP-56VTHAK>
2022-06-10 12:59:12 +0900

    컨트롤러, index 수정 2개추가 by hong

--- demo/src/main/java/com/ez/demo/controller/TestController.java
+++ demo/src/main/java/com/ez/demo/controller/TestController.java
@@ -1,6 +1,11 @@
 package com.ez.demo.controller;
 
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class TestController {
-
+	public String test() {
+		System.out.println("test by hong");
+		return "/index";
+	}
 }
